30f86ca91f6af4d59ffb50a7dd38e332035e3abb
Hanzo-00 <mm713323@gmail.com>
2024-08-30 13:41:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     const MAX_KEYS_PER_GAME_PER_DAY = 10;
-    //const EVENTS_DELAY = 20000;
+    //const EVENTS_DELAY = 50000;
 
     const games = {
         1: {
