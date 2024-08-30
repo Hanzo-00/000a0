b2f37686a4aa0015a4fb719142be3a818a99e136
Hanzo-00 <mm713323@gmail.com>
2024-08-30 12:31:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Gangs Wars',
             appToken: 'b6de60a0-e030-48bb-a551-548372493523',
             promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
-            eventsDelay: 20000,
+            eventsDelay: 30000,
             attemptsNumber: 16,
         }
     };
