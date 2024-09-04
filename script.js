00bc7b731fddc6645889c265aaf35c5acb42f1f7
Hanzo-00 <mm713323@gmail.com>
2024-09-04 17:36:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ document.addEventListener('DOMContentLoaded', () => {
             attemptsNumber: 16,
         },
         7: {
-            name: 'Cafe Dash',
-            appToken: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
-            promoId: 'bc0971b8-04df-4e72-8a3e-ec4dc663cd11',
+            name: 'Tile Trio',
+            appToken: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
+            promoId: 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
             eventsDelay: 20000,
             attemptsNumber: 16,
         },
@@ -61,11 +61,11 @@ document.addEventListener('DOMContentLoaded', () => {
             attemptsNumber: 16,
         },
         9: {
-            name: 'Gangs Wars',
-            appToken: 'b6de60a0-e030-48bb-a551-548372493523',
-            promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
-            eventsDelay: 40000,
-            attemptsNumber: 30,
+            name: 'Fluff Crusade',
+            appToken: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+            promoId: '112887b0-a8af-4eb2-ac63-d82df78283d9',
+            eventsDelay: 20000,
+            attemptsNumber: 16,
         }
     };
     
