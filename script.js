9b2e2e665c5e44703fcec28a7fb5b1f15a454487
Hanzo-00 <mm713323@gmail.com>
2024-09-12 22:58:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -73,6 +73,13 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
             eventsDelay: 20000,
             attemptsNumber: 16,
+        },
+        11: {
+            name: 'Bouncemasters',
+            appToken: 'bc72d3b9-8e91-4884-9c33-f72482f0db37',
+            promoId: 'bc72d3b9-8e91-4884-9c33-f72482f0db37',
+            eventsDelay: 20000,
+            attemptsNumber: 16,
         }
     };
     
